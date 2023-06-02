a6ee38b5d96eb7126b2bfc0cfcaefc2baac06b40
cesar chavez <cesar.a.chavez.h@gmail.com>
2023-06-01 19:55:49 -0600

    creacion de base de dato

--- CRUD_controler/models.py
+++ CRUD_controler/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 35)
+    last_name = models.CharField(max_length = 35)
+    correo = models.CharField(max_length = 100)
+    password = models.CharField(max_length=20)
+    hora_creacion = models.DateTimeField(auto_now=True)
+
+    pass
+
+class Proyectos(models.Model):
+    id_project = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    name_project = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    hora_creacion = models.DateTimeField(auto_now=True)
+    pass
+
+class Capitulos(models.Model):
+    id_capitulo = models.AutoField(primary_key=True)
+    id_project = models.ForeignKey(Proyectos,on_delete=models.CASCADE)
+    id_user = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    numero_del_capitulo = models.CharField(max_length=10)
+    lista_etiqueta_capitulo = models.CharField(max_length=100)
+    contenido_capitulo = models.TextField(max_length= 8000)
+    url_capitulo = models.CharField()
+
+    pass
