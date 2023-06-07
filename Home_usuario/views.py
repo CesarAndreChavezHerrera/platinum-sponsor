151bc164ece95998e10b46bad41fbdcbde5222ca
cesar chavez <cesar.a.chavez.h@gmail.com>
2023-06-07 10:45:51 -0600

    se añadio la coneccion entre los elementos

--- Home_usuario/views.py
+++ Home_usuario/views.py
@@ -5,4 +5,7 @@ def inicio(request):
     return render(request, "home.html")
     pass
 
+def inicio_visitante(request):
+    return render(request,"vista_visitante.html")
+
 # Create your views here.
--- Perfil_usuario/views.py
+++ Perfil_usuario/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def perfil(request):
+
+    return render(request,'perfil_test.html')
+    pass
 # Create your views here.
